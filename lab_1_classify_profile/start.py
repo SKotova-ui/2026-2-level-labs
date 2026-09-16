5f4db11aa173f50f6e19a2698344d6d8740bdf6c
SKotova <pamythene@gmail.com>
2026-09-16 19:41:34 +0300

    -

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -18,6 +18,8 @@ def main() -> None:
         stopwords = file.read().split("\n")
     with open("lab_1_classify_profile/assets/texts/en.txt", "r", encoding="utf-8") as file:
         en_text = file.read()
+    result = None
+    assert result, "Detection result is None"
 
 if __name__ == "__main__":
     main()
