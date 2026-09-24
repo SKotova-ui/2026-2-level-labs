62103f976136ebce5fa81699b3772e8b29b25ad1
SKotova <pamythene@gmail.com>
2026-09-24 20:51:50 +0300

    fixing code style

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -4,12 +4,12 @@ Language detection starter.
 
 # pylint: disable=unused-variable, duplicate-code
 
+from lab_1_classify_profile.main import create_language_profile, detect_language_by_mse
+
 def main() -> None:
     """
     Launches an implementation.
     """
-    from lab_1_classify_profile.main import create_language_profile, detect_language_by_mse
-
     with open("lab_1_classify_profile/assets/texts/de.txt", "r", encoding="utf-8") as file:
         de_text = file.read()
     with open("lab_1_classify_profile/assets/texts/unknown.txt", "r", encoding="utf-8") as file:
@@ -22,7 +22,7 @@ def main() -> None:
     en_prof = create_language_profile("en", en_text, stopwords)
     unknown_prof = create_language_profile("en", unknown_text, stopwords)
     if de_prof is None or en_prof is None or unknown_prof is None:
-        return None
+        return
     result = detect_language_by_mse(
         unknown_prof, en_prof, de_prof)
     assert result, "Detection result is None"
